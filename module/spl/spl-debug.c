abda055b67c67ff9167bb973469b739cfa4bdbd3
Jorgen Lundman <lundman@lundman.net>
2017-09-05 10:15:03 +0900

    Cleanup debug macros VERIFY and ASSERT
    VERIFY will always call panic, in both DEBUG and NDEBUG
    
    ASSERT will call printf in DEBUG, and compiled out in NDEBUG.
    
    Add "likely" and "unlikely" macros.
    
    ASSERTS are defined to also call assfail() (as per IllumOS API) to assist
    with dtrace probe attachment. (and dumping stack at ASSERT).
    
    dtrace -n 'assfail:entry{ stack(); }'

--- module/spl/spl-debug.c
+++ module/spl/spl-debug.c
@@ -1,7 +1,10 @@
 #include <sys/sysmacros.h>
 #include <spl-debug.h>
-#include <spl-trace.h>
-#include <spl-ctl.h>
+
 
 
 /* Debug log support enabled */
+__attribute__((noinline)) int assfail(char *str) __attribute__((optnone))
+{
+	return 1; // Must return true for ASSERT macro
+}
