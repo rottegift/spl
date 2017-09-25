f2b0dce2b538a95347963ee8be56c1ba87bbfd3d
Sean Doran <smd@use.net>
2017-09-25 04:09:26 +0100

    wrap macros to spot panic below kmem_findslab

--- module/spl/spl-kmem.c
+++ module/spl/spl-kmem.c
@@ -833,6 +833,25 @@ kmem_cache_applyall_id(void (*func)(kmem_cache_t *), taskq_t *tq, int tqflag)
 /*
  * Debugging support.  Given a buffer address, find its slab.
  */
+
+static boolean_t
+kmem_findslab_kmem_slab_member(kmem_slab_t *sp, void *buf)
+{
+	VERIFY3P(sp,!=,NULL);
+	VERIFY3P(buf,!=,NULL);
+	return(KMEM_SLAB_MEMBER(sp, buf));
+}
+
+static kmem_slab_t *
+kmem_findslab_avl_next(avl_tree_t *tree, kmem_slab_t *node)
+{
+	VERIFY3P(tree,!=,NULL);
+	VERIFY3P(node,!=,NULL);
+
+	return(AVL_NEXT(tree, node));
+}
+
+
 static kmem_slab_t *
 kmem_findslab(kmem_cache_t *cp, void *buf)
 {
@@ -844,15 +863,15 @@ kmem_findslab(kmem_cache_t *cp, void *buf)
 	mutex_enter(&cp->cache_lock);
 	for (sp = list_head(&cp->cache_complete_slabs); sp != NULL;
 		 sp = list_next(&cp->cache_complete_slabs, sp)) {
-		if (KMEM_SLAB_MEMBER(sp, buf)) {
+		if (kmem_findslab_kmem_slab_member(sp, buf)) {
 			mutex_exit(&cp->cache_lock);
 			return (sp);
 		}
 	}
 
 	for (sp = avl_first(&cp->cache_partial_slabs); sp != NULL;
-		 sp = AVL_NEXT(&cp->cache_partial_slabs, sp)) {
-		if (KMEM_SLAB_MEMBER(sp, buf)) {
+		 sp = kmem_findslab_avl_next(&cp->cache_partial_slabs, sp)) {
+		if (kmem_findslab_kmem_slab_member(sp, buf)) {
 			mutex_exit(&cp->cache_lock);
 			return (sp);
 		}
