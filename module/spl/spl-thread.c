167acca4fae860732356f29c4c6cf538567c7ab6
Sean Doran <smd@use.net>
2019-02-23 15:50:10 +0000

    apply precedence and other QOS to all threads we create

--- module/spl/spl-thread.c
+++ module/spl/spl-thread.c
@@ -65,125 +65,120 @@ spl_thread_create(
         if (result != KERN_SUCCESS)
 			return (NULL);
 
-		/* Improve the priority when asked to do so */
-		if (pri > minclsyspri) {
-			thread_precedence_policy_data_t policy = { 0 };
+	thread_precedence_policy_data_t policy = { 0 };
 
-			/* kernel priorities (osfmk/kern/sched.h)
-			 *
-			 * 96           Reserved (real-time)
-			 * 95           Kernel mode only
-			 *                              A
-			 *                              +
-			 *                      (16 levels)
-			 *                              +
-			 *                              V
-			 * 80           Kernel mode only
-			 * 79           System high priority
-			 *
-			 * spl/include/sys/sysmacros.h
-			 * #define maxclsyspri  95  MAXPRI_KERNEL
-			 * #define minclsyspri  80  MINPRI_KERNEL
-			 * #define defclsyspri  81  BASEPRI_KERNEL
-			 *
-			 * Calling policy.importance = 10 will create
-			 * a default pri (BASEPRI_KERNEL 81) at pri (91).
-			 *
-			 * So asking for pri (85) we do 85-81 = 4.
-			 *
-			 * Asking for minclsyspri gives us 80 - 81 = -1
-			 * Asking for defclsyspri gives us 81 - 81 = 0
-			 * Asking for maxclsyspri gives us 95 - 81 = 14
-			 *
-			 * IllumOS priorities are:
-			 * #define MAXCLSYSPRI     99
-			 * #define MINCLSYSPRI     60
-			 */
+	/* kernel priorities (osfmk/kern/sched.h)
+	 *
+	 * 96           Reserved (real-time)
+	 * 95           Kernel mode only
+	 *                              A
+	 *                              +
+	 *                      (16 levels)
+	 *                              +
+	 *                              V
+	 * 80           Kernel mode only
+	 * 79           System high priority
+	 *
+	 * spl/include/sys/sysmacros.h
+	 * #define maxclsyspri  95  MAXPRI_KERNEL
+	 * #define minclsyspri  80  MINPRI_KERNEL
+	 * #define defclsyspri  81  BASEPRI_KERNEL
+	 *
+	 * Calling policy.importance = 10 will create
+	 * a default pri (BASEPRI_KERNEL 81) at pri (91).
+	 *
+	 * So asking for pri (85) we do 85-81 = 4.
+	 *
+	 * Asking for minclsyspri gives us 80 - 81 = -1
+	 * Asking for defclsyspri gives us 81 - 81 = 0
+	 * Asking for maxclsyspri gives us 95 - 81 = 14
+	 *
+	 * IllumOS priorities are:
+	 * #define MAXCLSYSPRI     99
+	 * #define MINCLSYSPRI     60
+	 */
 
-			policy.importance = (pri - defclsyspri);
+	policy.importance = (pri - defclsyspri);
 
-			kern_return_t pol_prec_kret = thread_policy_set(thread,
-							  THREAD_PRECEDENCE_POLICY,
-							  (thread_policy_t)&policy,
-							  THREAD_PRECEDENCE_POLICY_COUNT);
-			if (pol_prec_kret != KERN_SUCCESS) {
-				printf("SPL: %s:%d: ERROR failed to set thread precedence to %d ret %d\n",
-				    __func__, __LINE__, pri - minclsyspri, pol_prec_kret);
-			}
-		}
+	kern_return_t pol_prec_kret = thread_policy_set(thread,
+	    THREAD_PRECEDENCE_POLICY,
+	    (thread_policy_t)&policy,
+	    THREAD_PRECEDENCE_POLICY_COUNT);
+	if (pol_prec_kret != KERN_SUCCESS) {
+		printf("SPL: %s:%d: ERROR failed to set thread precedence to %d ret %d\n",
+		    __func__, __LINE__, pri - minclsyspri, pol_prec_kret);
+	}
 
-		/* set TIMESHARE policy on our non-maxclsyspri; busiest
-		 * threads should decay to avoid hurting GUI
-		 * performance
-		 */
+	/* set TIMESHARE policy on our threads; busiest
+	 * threads should decay to avoid hurting GUI
+	 * performance
+	 */
 
-		if (pri < maxclsyspri) {
-			thread_extended_policy_data_t policy = { .timeshare = TRUE };
-			kern_return_t kret = thread_policy_set(thread,
-			    THREAD_EXTENDED_POLICY,
-			    (thread_policy_t)&policy,
-			    THREAD_EXTENDED_POLICY_COUNT);
-			if (kret != KERN_SUCCESS) {
-				printf("SPL: %s:%d: WARNING failed to set timeshare policy retval: %d\n",
-				    __func__, __LINE__, kret);
-			}
-		}
+	thread_extended_policy_data_t policy = { .timeshare = TRUE };
+	kern_return_t kret = thread_policy_set(thread,
+	    THREAD_EXTENDED_POLICY,
+	    (thread_policy_t)&policy,
+	    THREAD_EXTENDED_POLICY_COUNT);
+	if (kret != KERN_SUCCESS) {
+		printf("SPL: %s:%d: WARNING failed to set timeshare policy retval: %d\n",
+		    __func__, __LINE__, kret);
+	}
 
-		/* TIERs: 0 is USER_INTERACTIVE, 1 is USER_INITIATED, 2 is LEGACY,
-                 *        3 is UTILITY, 4 is BACKGROUND, 5 is MAINTENANCE
-		 */
+	/* TIERs: 0 is USER_INTERACTIVE, 1 is USER_INITIATED, 2 is LEGACY,
+	 *        3 is UTILITY, 4 is BACKGROUND, 5 is MAINTENANCE
+	 */
 
-		/* throughput: less than user_interactive in all cases,
-		 *             we can be maint for defclsyspri and lower
-		 */
+	/* throughput: less than user_interactive in all cases,
+	 *             we can be maint for defclsyspri and lower
+	 */
 
-		const thread_throughput_qos_t tput_high = THROUGHPUT_QOS_TIER_3;
-		const thread_throughput_qos_t tput_normal = THROUGHPUT_QOS_TIER_4;
-		const thread_throughput_qos_t tput_low = THROUGHPUT_QOS_TIER_5;
-		thread_throughput_qos_policy_data_t throughput_qos = { 0 };
-		if (pri >= maxclsyspri)
-			throughput_qos.thread_throughput_qos_tier = tput_high;
-		else if (pri > defclsyspri)
-			throughput_qos.thread_throughput_qos_tier = tput_normal;
-		else
-			throughput_qos.thread_throughput_qos_tier = tput_low;
+	const thread_throughput_qos_t tput_high = THROUGHPUT_QOS_TIER_3;
+	const thread_throughput_qos_t tput_normal = THROUGHPUT_QOS_TIER_4;
+	const thread_throughput_qos_t tput_low = THROUGHPUT_QOS_TIER_5;
+	thread_throughput_qos_policy_data_t throughput_qos = { 0 };
+	if (pri >= maxclsyspri)
+		throughput_qos.thread_throughput_qos_tier = tput_high;
+	else if (pri > defclsyspri)
+		throughput_qos.thread_throughput_qos_tier = tput_normal;
+	else
+		throughput_qos.thread_throughput_qos_tier = tput_low;
 
-		/* sysdc latency is 3
-		 * so we want to be a better QOS than that for normal threads,
-		 * and less than that for the lowest-priority ones
-		 */
+	/* sysdc latency is 3
+	 * so we want to be a better QOS than that for normal threads,
+	 * and less than that for the lowest-priority ones
+	 */
 
-		const thread_latency_qos_t latency_high = LATENCY_QOS_TIER_3;
-		const thread_latency_qos_t latency_normal = LATENCY_QOS_TIER_4;
-		const thread_latency_qos_t latency_low = LATENCY_QOS_TIER_5;
-		thread_latency_qos_policy_data_t latency_qos = { 0 };
-		if (pri >= maxclsyspri)
-			latency_qos.thread_latency_qos_tier = latency_high;
-		else if (pri > defclsyspri)
-			latency_qos.thread_latency_qos_tier = latency_normal;
-		else
-			latency_qos.thread_latency_qos_tier = latency_low;
+	const thread_latency_qos_t latency_high = LATENCY_QOS_TIER_3;
+	const thread_latency_qos_t latency_normal = LATENCY_QOS_TIER_4;
+	const thread_latency_qos_t latency_low = LATENCY_QOS_TIER_5;
+	thread_latency_qos_policy_data_t latency_qos = { 0 };
+	if (pri >= maxclsyspri)
+		latency_qos.thread_latency_qos_tier = latency_high;
+	else if (pri > defclsyspri)
+		latency_qos.thread_latency_qos_tier = latency_normal;
+	else
+		latency_qos.thread_latency_qos_tier = latency_low;
 
-		kern_return_t qoskret = thread_policy_set(thread,
-                    THREAD_THROUGHPUT_QOS_POLICY,
-                    (thread_policy_t)&throughput_qos,
-                    THREAD_THROUGHPUT_QOS_POLICY_COUNT);
-                if (qoskret != KERN_SUCCESS) {
-                        printf("SPL: %s:%d: WARNING failed to set thread throughput policy retval: %d "
-                            " (THREAD_THROUGHPUT_QOS_POLICY %x)",
-                            __func__, __LINE__, qoskret, throughput_qos.thread_throughput_qos_tier);
-		}
+	kern_return_t qoskret = thread_policy_set(thread,
+	    THREAD_THROUGHPUT_QOS_POLICY,
+	    (thread_policy_t)&throughput_qos,
+	    THREAD_THROUGHPUT_QOS_POLICY_COUNT);
+	if (qoskret != KERN_SUCCESS) {
+		printf("SPL: %s:%d: WARNING failed to set thread throughput policy retval: %d "
+		    " (THREAD_THROUGHPUT_QOS_POLICY %x)",
+		    __func__, __LINE__, qoskret, throughput_qos.thread_throughput_qos_tier);
+	}
 
-		qoskret = thread_policy_set(thread,
-		    THREAD_LATENCY_QOS_POLICY,
-		    (thread_policy_t)&latency_qos,
-		    THREAD_LATENCY_QOS_POLICY_COUNT);
-		if (qoskret != KERN_SUCCESS) {
-			printf("SPL: %s:%d: WARNING failed to set thread"
-			    " latency policy retval: %d latency policy %x\n",
-			    __func__, __LINE__,
-			    qoskret, latency_qos.thread_latency_qos_tier);
-		}
+	qoskret = thread_policy_set(thread,
+	    THREAD_LATENCY_QOS_POLICY,
+	    (thread_policy_t)&latency_qos,
+	    THREAD_LATENCY_QOS_POLICY_COUNT);
+	if (qoskret != KERN_SUCCESS) {
+		printf("SPL: %s:%d: WARNING failed to set thread"
+		    " latency policy retval: %d latency policy %x\n",
+		    __func__, __LINE__,
+		    qoskret, latency_qos.thread_latency_qos_tier);
+	}
 
         thread_deallocate(thread);
 
