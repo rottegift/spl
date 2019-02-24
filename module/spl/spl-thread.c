afd309951d70afe13e98f72deb75c724be592735
Sean Doran <smd@cla.use.net>
2019-02-24 01:58:37 +0000

    boost latency tiers again

--- module/spl/spl-thread.c
+++ module/spl/spl-thread.c
@@ -127,7 +127,7 @@ spl_thread_create(
 	 */
 
 	const thread_throughput_qos_t tput_high = THROUGHPUT_QOS_TIER_1;
-	const thread_throughput_qos_t tput_normal = THROUGHPUT_QOS_TIER_2;
+	const thread_throughput_qos_t tput_normal = THROUGHPUT_QOS_TIER_3;
 	const thread_throughput_qos_t tput_low = THROUGHPUT_QOS_TIER_4;
 	thread_throughput_qos_policy_data_t throughput_qos = { 0 };
 	if (pri >= maxclsyspri)
@@ -142,9 +142,9 @@ spl_thread_create(
 	 * and less than that for the lowest-priority ones
 	 */
 
-	const thread_latency_qos_t latency_high = LATENCY_QOS_TIER_1;
-	const thread_latency_qos_t latency_normal = LATENCY_QOS_TIER_2;
-	const thread_latency_qos_t latency_low = LATENCY_QOS_TIER_4;
+	const thread_latency_qos_t latency_high = LATENCY_QOS_TIER_0;
+	const thread_latency_qos_t latency_normal = LATENCY_QOS_TIER_1;
+	const thread_latency_qos_t latency_low = LATENCY_QOS_TIER_1;
 	thread_latency_qos_policy_data_t latency_qos = { 0 };
 	if (pri >= maxclsyspri)
 		latency_qos.thread_latency_qos_tier = latency_high;
